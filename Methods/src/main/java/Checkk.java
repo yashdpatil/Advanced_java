00f44827d170b035a6cdc63cdd4d08ad1c88a4b1
YashDPatil <yashpatil1711@gmail.com>
2023-12-18 22:34:53 +0530

    first commit

---
+++ Methods/src/main/java/Checkk.java
@@ -0,0 +1,34 @@
+
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class Checkk
+ */
+public class Checkk extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Checkk() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
